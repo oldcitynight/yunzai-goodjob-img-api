96fa4d56df1fa33fcfe0781dee08561c0db4be3a
OldCityNight <nightoldcity@gmail.com>
2024-07-09 21:41:06 +0800

    不用 IDE 写 golang 导致的(雾)

--- golang/main.go
+++ golang/main.go
@@ -171,14 +171,14 @@ func help(_gin *gin.Context) {
 
 // 从 git 更新图片
 func updateImg() {
-    // 确认图片资源存在
-    if _, err := os.Stat("./goodjob-img"); os.IsNotExist(err) {
-        cmd := exec.Command("git", "clone", res_path)
-        output, err := cmd.CombinedOutput()
-        if err != nil {
-            fmt.Println(err)
-        }
-    }
+	// 确认图片资源存在
+	if _, err := os.Stat("./goodjob-img"); os.IsNotExist(err) {
+		cmd := exec.Command("git", "clone", res_path)
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 	cmd := exec.Command("git", "pull", "-f")
 	cmd.Dir = "./goodjob-img"
 	output, err := cmd.CombinedOutput()
